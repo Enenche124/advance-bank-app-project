faa8e9bad1af5823ca69647fe856811b5268d39e
Ericalli <Ericalli>
2025-07-06 08:05:35 +0100

    committed

--- src/main/java/com/apostle/data/model/Transaction.java
+++ src/main/java/com/apostle/data/model/Transaction.java
@@ -1,4 +1,24 @@
 package com.apostle.data.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Data
+@Entity
+@Table(name = "transaction")
 public class Transaction {
+    @Id
+    private Long transactionId;
+    private Long senderId;
+    private Long receiverId;
+    private BigDecimal amount;
+    private LocalDateTime timestamp;
+    private TransactionStatus status;
+    private TransactionType type;
+    private String note;
+
 }
