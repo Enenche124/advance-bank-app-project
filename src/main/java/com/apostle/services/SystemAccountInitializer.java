26737ceda7afba62febe51e900c4f8155c5ee8c7
enenche-001 <enenchejohn@gmail.com>
2025-07-10 11:14:57 +0100

    refactor

--- src/main/java/com/apostle/services/SystemAccountInitializer.java
+++ src/main/java/com/apostle/services/SystemAccountInitializer.java
@@ -1,19 +1,19 @@
-package com.apostle.services;
-
-import com.apostle.data.model.AccountType;
-import com.apostle.data.model.BankAccount;
-import com.apostle.data.repositories.BankAccountRepository;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-
-@Component
-@RequiredArgsConstructor
-public class SystemAccountInitializer {
-    private final BankAccountRepository bankAccountRepository;
-
+//package com.apostle.services;
+//
+//import com.apostle.data.model.AccountType;
+//import com.apostle.data.model.BankAccount;
+//import com.apostle.data.repositories.BankAccountRepository;
+//import jakarta.annotation.PostConstruct;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//
+//import java.math.BigDecimal;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class SystemAccountInitializer {
+//    private final BankAccountRepository bankAccountRepository;
+//
 //    @PostConstruct
 //    public void createSystemAccountIfNotExists() {
 //        Long systemId = 0L;
@@ -29,27 +29,27 @@ public class SystemAccountInitializer {
 //
 //        }
 //    }
-
-    @PostConstruct
-    public void createSystemAccountIfNotExists() {
-        try {
-            Long systemId = 0L;
-            if (!bankAccountRepository.existsById(systemId)) {
-                BankAccount systemAccount = BankAccount.builder()
-                        .id(systemId)
-                        .accountNumber("SYSTEM")
-                        .name("System Account")
-                        .balance(BigDecimal.ZERO)
-                        .accountType(AccountType.SYSTEM)
-                        .user(null)  // Should be fine if DB allows null
-                        .build();
-                bankAccountRepository.save(systemAccount);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("Failed to initialize system account", e);
-        }
-    }
-
-
-}
+//
+//    @PostConstruct
+//    public void createSystemAccountIfNotExists() {
+//        try {
+//            Long systemId = 0L;
+//            if (!bankAccountRepository.existsById(systemId)) {
+//                BankAccount systemAccount = BankAccount.builder()
+//                        .id(systemId)
+//                        .accountNumber("SYSTEM")
+//                        .name("System Account")
+//                        .balance(BigDecimal.ZERO)
+//                        .accountType(AccountType.SYSTEM)
+//                        .user(null)  // Should be fine if DB allows null
+//                        .build();
+//                bankAccountRepository.save(systemAccount);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw new RuntimeException("Failed to initialize system account", e);
+//        }
+//    }
+//
+//
+//}
